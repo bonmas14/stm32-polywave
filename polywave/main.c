1bd3e3c78f777bfd40ff7cec38e662a5e0ed1160
bonmas <bonmas14@gmail.com>
2024-06-03 10:44:34 -0400

    struggles with isr

--- polywave/main.c
+++ polywave/main.c
@@ -1,37 +1,43 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// todo: add tim2 as irq instead of tim3 and sync them
+// not working because isr is too long?
+// 
+
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 
+#define FREQ_ARR (300 - 1)
+#define OUT_OFFSET ((FREQ_ARR - 255) / 2)
+
+#define BUFF_SIZE 512
+
 void init_mcu(void);
-void init_carrier(void);
-void init_sampler(void);
+void init_timers(void);
+
+size_t buff_index = 0;
+uint8_t buffer[BUFF_SIZE];
 
 int main(void) {
-    init_mcu();
-
-    size_t j = 0;
-    int8_t d = 1;
-
-    while (1) {
-        if (j == 256)
-            d = -1;
-        else if (j == 0)
-            d = 1;
-
-        j += d;
-
-        TIM3_CCR1 = j;
-
-        gpio_toggle(GPIOC, GPIO13);
-
-        for (size_t i = 0; i < 180000; i++) {
-            __asm__("nop");
-        }
+    for (size_t i = 0; i < BUFF_SIZE; i++) {
+        buffer[i] = i / 2;
     }
 
+    init_mcu();
+
+    while (1) {
+        for (size_t i = 0; i < (18000000) / (255); i++) {
+            __asm__("nop");
+        }
+
+        TIM3_CCR1 = buffer[buff_index++] + OUT_OFFSET;  
+        if (buff_index >= BUFF_SIZE)
+            buff_index = 0;
+        gpio_toggle(GPIOC, GPIO13);
+    }
 }
 
 void init_mcu(void) {
@@ -42,38 +48,32 @@ void init_mcu(void) {
     rcc_periph_clock_enable(RCC_GPIOA); 
     rcc_periph_clock_enable(RCC_GPIOC);
 
-    init_carrier();
-
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
-    
+
+    //nvic_enable_irq(NVIC_TIM3_IRQ);
+    //nvic_set_priority(NVIC_TIM3_IRQ, 1);
+
+    init_timers();
 }
 
-
-void init_carrier(void) {
+void init_timers(void) {
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_TIM3_CH1);
 
     TIM3_CR1 = TIM_CR1_CKD_CK_INT | TIM_CR1_CMS_EDGE;
-    TIM3_ARR = (1 << 8) - 1;
+    TIM3_CNT = 0;
+    TIM3_ARR = FREQ_ARR; 
+    TIM3_PSC = 4; // 4 = 48khz 9 = 24khz (5, 10)
+    TIM3_CCR1 = 127 + OUT_OFFSET;
 
-    TIM3_PSC = 0;
     TIM3_EGR = TIM_EGR_UG;
-
     TIM3_CCMR1 |= TIM_CCMR1_OC1M_PWM1 | TIM_CCMR1_OC1PE;
     TIM3_CCER |= TIM_CCER_CC1P | TIM_CCER_CC1E;
 
-    TIM3_CCR1 = 0;
-
     TIM3_CR1 |= TIM_CR1_ARPE;
+    TIM3_CR1 &= ~TIM_CR1_OPM;
+
+    //TIM3_DIER |= TIM_DIER_UIE; // do this with TIM2 and sync them togethe
+    
     TIM3_CR1 |= TIM_CR1_CEN;
 }
 
-void init_sampler(void) {
-
-}
-
-
-// 1 таймер - генерация несущего сигнала
-// 2 таймер - генерация и микширование "аналоговых" сигналов
-//  прерывание на таймер 48кГц
-//
-// +midi event
